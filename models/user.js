639274d05feab7ae3ba0299a606b23e99c598e8e
Derian Rambo <rambod@oregonstate.edu>
2023-06-14 01:01:34 -0700

    more work on user model

--- models/user.js
+++ models/user.js
@@ -4,8 +4,18 @@ const sequelize = require('../lib/sequelize')
 const bcrypt = require('bcrypt')
 
 const User = sequelize.define('user', {
-    name: {},
-    email {},
-    password {},
-    role {}
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false, set(value) {
+        this.setDataValue('password', bcrypt.hashSync(value, 8))
+    } },
+    role: { type: DataTypes.ENUM('admin', 'instructor', 'student'), allowNull: false }
 })
+
+exports.User = User
+
+exports.UserClientFields = [
+    'name',
+    'email',
+    'password'
+]
