58e6dd4a2181c6157b90e9a3cfe6ef352a73a077
Lawson Dietz <59808689+dietzl@users.noreply.github.com>
2023-06-14 23:44:43 +0000

    Adding API endpoints without authentication

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 
@@ -44,4 +45,3 @@ sequelize.sync().then(function () {
     console.log("== Server is running on port", port);
   });  
 })
-
